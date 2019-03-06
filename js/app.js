0bcca588a8f2cd9790cf035fa2e3e650bcb169de
timenyin <timenyindonkunu@gmail.com>
2019-03-07 00:26:40 +0100

    interiors furnteres

---
+++ js/app.js
@@ -0,0 +1,6 @@
+document.getElementById("cart-info").addEventListener("click", function() {
+  const cart = document.getElementById("cart");
+  cart.classList.toggle("show-cart");
+  console.log(cart);
+    
+});
